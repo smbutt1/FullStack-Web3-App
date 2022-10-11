3fd4019afc082eaecd1dbb352307bb8d65cb57c3
smbutt1 <98937511+smbutt1@users.noreply.github.com>
2022-10-11 20:41:30 +0500

    Folder Structure and final commit

--- client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-uri: "YOUR_GRAPHQL_URL_HERE",
-cache: new InMemoryCache(),
-});
-
-export default client;
